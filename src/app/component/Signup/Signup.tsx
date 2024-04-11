8449c432ffca697d2dca47b860f28812b0071856
okosun <okosunmicheal123@gmail.com>
2024-04-10 22:19:37 -0600

    image

---
+++ src/app/component/Signup/Signup.tsx
@@ -0,0 +1,30 @@
+  import Image from "next/image";     
+import signup from "../"
+
+
+
+
+export default function Signup() {
+  return (
+    <div className=" flex">  
+<div>   
+
+
+
+
+</div>
+
+<div>  
+  
+
+<Image src={signup} alt="signup" width={500} height={300} />
+
+   
+</div>
+
+
+    </div>
+
+
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Signup from "./component/Signup/Signup";
+
+
+
+
+export default function Home() {
+  return (
+    <div>   
+<Signup/>
+    </div>
+    
+  );
+}
