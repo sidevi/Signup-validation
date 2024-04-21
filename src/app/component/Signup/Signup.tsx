6ef7d12c2bb297d34ff57ee223501a4fcfae41d3
okosun <okosunmicheal123@gmail.com>
2024-04-21 01:03:02 -0600

    im finish it

--- src/app/component/Signup/Signup.tsx
+++ src/app/component/Signup/Signup.tsx
@@ -9,7 +9,7 @@ export default function Signup() {
   <section className="lg:bg-gray-50 min-h-screen flex items-center justify-center ">    
   
   
-  <div className="relative bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center overflow-hidden ">
+  <div className="relative bg-gray-100 flex rounded-2xl shadow-lg max-w-3xl p-5 items-center overflow-hidden  sm:flex p-13">
 
   
       <div className=" z-10 ">
@@ -23,7 +23,7 @@ export default function Signup() {
       </div>
 
     
-      <div className="absolute top-0 right-0 z-0 ">
+      <div className=" absolute top-0 right-0 z-0 ">
         <Image
           src={signup}
           alt="signup"
